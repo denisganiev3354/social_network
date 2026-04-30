4f70a02eb5aa21435bc4898e053b89c548925b98
Casper3354 <denisganiev3354@gmail.com>
2026-04-30 21:11:07 +0300

    code is run

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,19 @@
 import { resolve } from 'node:path';
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
     build: {
         outDir: 'static',
         emptyOutDir: false,
+        sourcemap: true,
         rolldownOptions: {
-            input: resolve(import.meta.dirname, 'src/master.js'),
+            input: resolve(import.meta.dirname, 'src/master.jsx'),
             output: {
                 entryFileNames: '[name].js',
                 assetileNames: '[name].[ext]'
             }
         }
-    }
+    },
+    plugins: [react()]
 });
